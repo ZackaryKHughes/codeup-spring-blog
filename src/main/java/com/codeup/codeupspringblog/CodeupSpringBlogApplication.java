1af131ce85e13aed75e5c8186702fe8ecd7a610f
Zackary Hughes <zackary.k.hughes@gmail.com>
2023-08-07 10:42:39 -0500

    Finished controller exercises

--- src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
+++ src/main/java/com/codeup/codeupspringblog/CodeupSpringBlogApplication.java
@@ -2,6 +2,10 @@ package com.codeup.codeupspringblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class CodeupSpringBlogApplication {
@@ -9,5 +13,22 @@ public class CodeupSpringBlogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CodeupSpringBlogApplication.class, args);
 	}
+	@Controller
+	static
+	class HelloController {
+
+		@GetMapping("/hello")
+		@ResponseBody
+		public String hello() {
+			return "Hello from Spring!";
+		}
+	}
+
+	@GetMapping("/hello/{name}")
+	@ResponseBody
+	public String sayHello(@PathVariable String name) {
+		return "Hello " + name + "!";
+	}
+
 
 }
